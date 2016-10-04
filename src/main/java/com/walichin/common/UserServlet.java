38af088ee026cc13b92ae5f86a098d0e5a666c9e
walichin <walter.alejos@gmail.com>
2016-10-03 22:31:36 -0700

    first commit

---
+++ src/main/java/com/walichin/common/UserServlet.java
@@ -0,0 +1,50 @@
+package com.walichin.common;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+import org.hibernate.Session;
+import com.walichin.persistence.HibernateUtil;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+        
+		System.out.println("Maven + Hibernate + MySQL");
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+        User user = new User();
+
+        user.setUser_Name("Pancho Villa");
+        user.setUser_Login("pvilla");
+
+        session.save(user);
+        session.getTransaction().commit();
+		
+		
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
